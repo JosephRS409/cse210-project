435cfd34ebddcef2de82cd3c873fb6df2c06be94
Austin Jesperson <jes16003@byui.edu>
2021-07-08 18:26:08 -0600

    Tile sets

---
+++ finding_dallin/assets/dungeon_tiles_compact_and_varied.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="dungeon_tiles_compact_and_varied" tilewidth="16" tileheight="16" tilecount="126" columns="21">
+ <image source="../../Dungeon tileset _ OpenGameArt.org_files/dungeon_tiles_compact_and_varied.png" width="336" height="104"/>
+</tileset>
