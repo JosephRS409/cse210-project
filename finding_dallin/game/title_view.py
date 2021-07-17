2614ff350ddecc667cf418461eecba507eb72da6
AlanWangsgard <alanwangsgard@byui.edu>
2021-07-17 12:20:30 -0600

    yeet

--- finding_dallin/game/title_view.py
+++ finding_dallin/game/title_view.py
@@ -1,6 +1,7 @@
 
 import arcade
 from game import constants
+from game.dallin import Dallin
 from game.screen import Show_screen
 
 class Title(arcade.View):
@@ -9,10 +10,16 @@ class Title(arcade.View):
         super().__init__()
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
-        self.number = 1
+        self.texture = arcade.load_texture(constants.DALLIN)
+        self.top = 100
+        self.bottom = 100
 
     def setup(self):
         pass
+
+    # def on_update(self):
+    #     self.top += 10
+    #     self.bottom += 10
 
     def on_draw(self):
         arcade.start_render()
@@ -20,6 +27,7 @@ class Title(arcade.View):
                          constants.SCREEN_HEIGHT/2, arcade.color.CRIMSON, font_size=50, anchor_x="center")
         arcade.draw_text("Joseph, Alan, Austin", constants.SCREEN_WIDTH / 2,
                          constants.SCREEN_HEIGHT/2 - 50, arcade.color.CRIMSON, font_size=20, anchor_x="center")
+        self.texture.draw_sized(self.top,self.bottom,64,64)
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         show_view = Show_screen()
